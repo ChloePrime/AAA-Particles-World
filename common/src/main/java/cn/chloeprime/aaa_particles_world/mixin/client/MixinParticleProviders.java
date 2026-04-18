650323f160b867c0b3f5f425ef1619cc3b3c32a4
Chloe Prime <chloe_von_einzbern@qq.com>
2026-04-19 02:09:09 +0800

    修复内部类 Mixin 和 gradle 模板展开冲突
    (cherry picked from commit 209950e508c65a57c372001f8f5c08b3641184f4)

--- common/src/main/java/cn/chloeprime/aaa_particles_world/mixin/client/MixinParticleProviders.java
+++ common/src/main/java/cn/chloeprime/aaa_particles_world/mixin/client/MixinParticleProviders.java
@@ -11,40 +11,42 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+@SuppressWarnings("unused")
 public class MixinParticleProviders {
-    @Mixin(HugeExplosionParticle.Provider.class)
-    public static class Explosion {
-        @Inject(
-                method = "createParticle(Lnet/minecraft/core/particles/SimpleParticleType;Lnet/minecraft/client/multiplayer/ClientLevel;DDDDDD)Lnet/minecraft/client/particle/Particle;",
-                at = @At("HEAD"),
-                cancellable = true)
-        private void replaceWithEffekParticle(SimpleParticleType simpleParticleType, ClientLevel level, double x, double y, double z, double dx, double dy, double dz, CallbackInfoReturnable<Particle> cir) {
-            if (ExplosionEffek.isEnabled()) {
-                var wrapped = ExplosionEffek.createParticleWrapper(
-                        ExplosionEffek.Type.SMALL, level,
-                        x, y, z,
-                        dx, dy, dz,
-                        ExplosionEffek.SMALL_VANILLA_EXPLOSION_SIZE);
-                cir.setReturnValue(wrapped);
-            }
+}
+
+@Mixin(HugeExplosionParticle.Provider.class)
+class MixinHugeExplosionParticleProvider {
+    @Inject(
+            method = "createParticle(Lnet/minecraft/core/particles/SimpleParticleType;Lnet/minecraft/client/multiplayer/ClientLevel;DDDDDD)Lnet/minecraft/client/particle/Particle;",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void replaceWithEffekParticle(SimpleParticleType simpleParticleType, ClientLevel level, double x, double y, double z, double dx, double dy, double dz, CallbackInfoReturnable<Particle> cir) {
+        if (ExplosionEffek.isEnabled()) {
+            var wrapped = ExplosionEffek.createParticleWrapper(
+                    ExplosionEffek.Type.SMALL, level,
+                    x, y, z,
+                    dx, dy, dz,
+                    ExplosionEffek.SMALL_VANILLA_EXPLOSION_SIZE);
+            cir.setReturnValue(wrapped);
         }
     }
+}
 
-    @Mixin(HugeExplosionSeedParticle.Provider.class)
-    public static class LargeExplosionEmitter {
-        @Inject(
-                method = "createParticle(Lnet/minecraft/core/particles/SimpleParticleType;Lnet/minecraft/client/multiplayer/ClientLevel;DDDDDD)Lnet/minecraft/client/particle/Particle;",
-                at = @At("HEAD"),
-                cancellable = true)
-        private void replaceWithEffekParticle(SimpleParticleType simpleParticleType, ClientLevel level, double x, double y, double z, double dx, double dy, double dz, CallbackInfoReturnable<Particle> cir) {
-            if (ExplosionEffek.isEnabled()) {
-                var wrapped = ExplosionEffek.createParticleWrapper(
-                        ExplosionEffek.Type.BIG, level,
-                        x, y, z,
-                        dx, dy, dz,
-                        ExplosionEffek.LARGE_VANILLA_EXPLOSION_SIZE);
-                cir.setReturnValue(wrapped);
-            }
+@Mixin(HugeExplosionSeedParticle.Provider.class)
+class MixinHugeExplosionSeedParticleProvider {
+    @Inject(
+            method = "createParticle(Lnet/minecraft/core/particles/SimpleParticleType;Lnet/minecraft/client/multiplayer/ClientLevel;DDDDDD)Lnet/minecraft/client/particle/Particle;",
+            at = @At("HEAD"),
+            cancellable = true)
+    private void replaceWithEffekParticle(SimpleParticleType simpleParticleType, ClientLevel level, double x, double y, double z, double dx, double dy, double dz, CallbackInfoReturnable<Particle> cir) {
+        if (ExplosionEffek.isEnabled()) {
+            var wrapped = ExplosionEffek.createParticleWrapper(
+                    ExplosionEffek.Type.BIG, level,
+                    x, y, z,
+                    dx, dy, dz,
+                    ExplosionEffek.LARGE_VANILLA_EXPLOSION_SIZE);
+            cir.setReturnValue(wrapped);
         }
     }
 }
