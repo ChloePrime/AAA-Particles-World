cff0c0d4b56faab57da0d6ed516b239b657aa687
Chloe Prime <chloe_von_einzbern@qq.com>
2026-04-19 20:49:44 +0800

    让原版设置里的粒子效果设置为最少时关闭 AAAPW 的 Effek
    (cherry picked from commit 420aafaa9d745bc1c9c78c8beba86ea3367e2187)

--- common/src/main/java/cn/chloeprime/aaa_particles_world/client/AAAParticlesWorldClient.java
+++ common/src/main/java/cn/chloeprime/aaa_particles_world/client/AAAParticlesWorldClient.java
@@ -4,11 +4,24 @@ import cn.chloeprime.aaa_particles_world.client.content.AtmosphereEffekManager;
 import mod.chloeprime.aaaparticles.client.installer.NativePlatform;
 import net.minecraft.client.GraphicsStatus;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.ParticleStatus;
 
 public class AAAParticlesWorldClient {
+    public static boolean isEffekReduced() {
+        return particleOption() != ParticleStatus.ALL;
+    }
+
     public static boolean isEffekEnabled() {
+        // 粒子效果为最少时关闭 Effek
+        if (particleOption() == ParticleStatus.MINIMAL) {
+            return false;
+        }
         boolean badOS = NativePlatform.isRunningOnUnsupportedPlatform();
         return !badOS && Minecraft.getInstance().options.graphicsMode().get() != GraphicsStatus.FABULOUS;
+    }
+
+    private static ParticleStatus particleOption() {
+        return Minecraft.getInstance().options.particles().get();
     }
 
     public static void init() {
