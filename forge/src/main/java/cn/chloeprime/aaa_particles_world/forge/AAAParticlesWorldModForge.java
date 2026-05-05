8cda7f3196dc8fa577eaa68d194e1a3c2f3a4ac5
Chloe Prime <chloe_von_einzbern@qq.com>
2026-05-05 19:35:24 +0800

    [1.21.1+] NeoForge 迁移至多入口点架构，添加配置文件界面。

--- forge/src/main/java/cn/chloeprime/aaa_particles_world/forge/AAAParticlesWorldModForge.java
+++ forge/src/main/java/cn/chloeprime/aaa_particles_world/forge/AAAParticlesWorldModForge.java
@@ -1,18 +1,11 @@
 package cn.chloeprime.aaa_particles_world.forge;
 
 import cn.chloeprime.aaa_particles_world.AAAParticlesWorldMod;
-import cn.chloeprime.aaa_particles_world.forge.client.AAAParticlesWorldModForgeClient;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(AAAParticlesWorldMod.MOD_ID)
 public final class AAAParticlesWorldModForge {
     public AAAParticlesWorldModForge() {
         AAAParticlesWorldMod.init();
-
-        if (FMLLoader.getCurrent().getDist().isClient()) {
-            AAAParticlesWorldModForgeClient.onInitializeClient(ModLoadingContext.get());
-        }
     }
 }
--- forge/src/main/java/cn/chloeprime/aaa_particles_world/forge/client/AAAParticlesWorldModForgeClient.java
+++ forge/src/main/java/cn/chloeprime/aaa_particles_world/forge/client/AAAParticlesWorldModForgeClient.java
@@ -1,13 +1,20 @@
 package cn.chloeprime.aaa_particles_world.forge.client;
 
+import cn.chloeprime.aaa_particles_world.AAAParticlesWorldMod;
 import cn.chloeprime.aaa_particles_world.client.AAAParticlesWorldClient;
 import cn.chloeprime.aaa_particles_world.client.ClientConfig;
-import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
+@Mod(value = AAAParticlesWorldMod.MOD_ID, dist = Dist.CLIENT)
 public final class AAAParticlesWorldModForgeClient {
-    public static void onInitializeClient(ModLoadingContext context) {
-        context.getActiveContainer().registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
+    public AAAParticlesWorldModForgeClient(ModContainer container) {
+        container.registerConfig(ModConfig.Type.CLIENT, ClientConfig.SPEC);
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
         AAAParticlesWorldClient.init();
     }
 }
