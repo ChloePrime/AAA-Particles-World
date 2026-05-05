59bc9c023c954d05a38e2525da6cd5904be05f11
Chloe Prime <chloe_von_einzbern@qq.com>
2026-05-05 19:35:24 +0800

    [1.21.1+] NeoForge 迁移至多入口点架构，添加配置文件界面。

--- forge/src/main/java/cn/chloeprime/aaa_particles_world/forge/AAAParticlesWorldModForge.java
+++ forge/src/main/java/cn/chloeprime/aaa_particles_world/forge/AAAParticlesWorldModForge.java
@@ -1,18 +1,11 @@
 package cn.chloeprime.aaa_particles_world.forge;
 
 import cn.chloeprime.aaa_particles_world.AAAParticlesWorldMod;
-import cn.chloeprime.aaa_particles_world.forge.client.AAAParticlesWorldModForgeClient;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforgespi.Environment;
 
 @Mod(AAAParticlesWorldMod.MOD_ID)
 public final class AAAParticlesWorldModForge {
     public AAAParticlesWorldModForge() {
         AAAParticlesWorldMod.init();
-
-        if (Environment.get().getDist().isClient()) {
-            AAAParticlesWorldModForgeClient.onInitializeClient(ModLoadingContext.get());
-        }
     }
 }
