78434a64da18709881ebc7bf069b888e7fef850e
Chloe Prime <chloe_von_einzbern@qq.com>
2026-07-12 06:10:50 +0800

    修复 gunsmith lib 多重爆炸的 bug

--- common/src/main/java/cn/chloeprime/aaa_particles_world/mixin/MixinExplosion.java
+++ common/src/main/java/cn/chloeprime/aaa_particles_world/mixin/MixinExplosion.java
@@ -7,6 +7,7 @@ import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +17,13 @@ public class MixinExplosion {
     @WrapOperation(
             method = "finalizeExplosion",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addParticle(Lnet/minecraft/core/particles/ParticleOptions;DDDDDD)V"))
-    private void betterExplodeParticles(Level level, ParticleOptions vanillaExplosion, double x, double y, double z, double dx, double dy, double dz, Operation<Void> original) {
+    private void betterExplodeParticles(
+            Level level, ParticleOptions vanillaExplosion, double x, double y, double z, double dx, double dy, double dz, Operation<Void> original,
+            boolean spawnParticles
+    ) {
+        if (!spawnParticles) {
+            return;
+        }
         var vanilla = (Runnable) () -> original.call(level, vanillaExplosion, x, y, z, dx, dy, dz);
         if (!ExplosionEffek.isEnabled()) {
             vanilla.run();
@@ -39,5 +46,5 @@ public class MixinExplosion {
         ExplosionEffek.playExplosion(type, level, x, y, z, radius);
     }
 
-    @Shadow public float radius;
+    @Shadow @Final private float radius;
 }
